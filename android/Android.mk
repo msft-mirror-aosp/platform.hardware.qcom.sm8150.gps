8c2c5e8072dd227291bc20b56cd0d01840bd9b8d
Shinru Han <shinruhan@google.com>
2019-03-15 12:45:27 +0800

    Revert to GNSS HAL 1.1
    As HIDL 2.0 is not finalized, revert to HIDL 1.1.
    
    Bug: 128645355
    Test: build & gps sanity test
    Change-Id: I63949905b74fab70c3a90a78af85cfaf8ce6167a

--- android/Android.mk
+++ android/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifeq ($(GNSS_HIDL_VERSION),2.0)
-include $(LOCAL_PATH)/2.0/Android.mk
-else
 ifeq ($(GNSS_HIDL_VERSION),1.1)
 include $(LOCAL_PATH)/1.1/Android.mk
 else
@@ -10,6 +7,4 @@ include $(LOCAL_PATH)/1.0/Android.mk
 endif
 else #QMAA flag set, build dummy android.hardware.gnss@1.0-impl-qti
 include $(LOCAL_PATH)/dummy/Android.mk
-endif
-
 endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
